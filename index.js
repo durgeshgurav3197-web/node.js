c21fa59bd88f47df70285aeb901195ee8374a726
root <root@ip-172-31-36-40.us-east-2.compute.internal>
2026-01-18 12:35:33 +0000

    this is 3rd commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+console.log("App started");
+
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+const user = "World";
+console.log(greet(user));
+
+// Example loop
+for (let i = 1; i <= 5; i++) {
+  console.log("Count:", i);
+}
+
